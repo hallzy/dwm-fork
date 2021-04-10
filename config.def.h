28e83cd8768036b2e9b995d4ad30b16611fdac13
Steven Hall <steven.hall@sendtonews.com>
2021-04-09 17:14:21 -0700

    Emoji support

--- config.def.h
+++ config.def.h
@@ -35,7 +35,10 @@ static const unsigned int gappov    = GAP;       /* vert outer gap between windo
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = {
+	"monospace:size=10",
+	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
